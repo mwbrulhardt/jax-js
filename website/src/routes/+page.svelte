7a255a2aaa5f8a9d35267759c08b238f54913a9e
Eric Zhang <ekzhang1@gmail.com>
2025-02-27 21:05:41 -0500

    Change tone of pitch again

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -13,25 +13,24 @@
   </div>
 
   <p class="mb-5">
-    In 2025, most software is written in JavaScript and runs in browsers—but ML
-    libraries don't.
+    Machine learning belongs in the browser—instantly accessible, and running on
+    local GPUs. But existing libraries weren't <em>designed for</em> the web.
   </p>
-
-  <p class="mb-5">What if they did?</p>
 
   <pre class="mb-5 text-center"><code>npm install @jax-js/core</code></pre>
 
   <p class="mb-5">
-    <code>jax-js</code> aims to bring familiar array APIs and
+    <code>jax-js</code> brings familiar array APIs and
     <a
       class="link"
       target="_blank"
       href="https://en.wikipedia.org/wiki/Automatic_differentiation">autograd</a
-    > to the browser. So you can explore neural networks (AI), statistics, image
-    processing, and computational science on the web platform.
+    > to JavaScript. So you can explore neural networks (AI), statistics, image processing,
+    and computational science, all on the web platform.
   </p>
 
   <div class="mb-5 grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4">
+    <!-- TODO: Images -->
     <div class="aspect-video bg-gray-500">
       <span class="text-white text-xs italic">julia set</span>
     </div>
@@ -47,33 +46,41 @@
   </div>
 
   <p class="mb-5">
-    Like the Python libraries it's based on (NumPy, PyTorch, JAX), <code
+    Like the Python libraries it's inspired by (NumPy, PyTorch, JAX), <code
       >jax-js</code
     >
     is carefully hand-optimized, including a JIT compiler and scheduler for GPU kernels.
     But it's also
-    <strong>extremely simple and portable across the web</strong>. It runs on
-    Chrome, Firefox, Safari, Arc, iOS, and Android.
+    <strong>extremely simple and portable across the web</strong>, running on
+    Chrome, Firefox, Safari, iOS, and Android. On each platform, the compiler
+    generates specialized kernels on a variety of hardware.
   </p>
 
   <div class="mb-5 border h-48 flex items-center justify-center square-grid">
     <p class="text-sm italic text-center">
+      <!-- TODO: Insert graph -->
       Performance graph of flops: cpu, wasm, webgl, webgpu
     </p>
   </div>
 
   <p class="mb-5">
     How is this possible? In short: because of modern technologies like
-    WebAssembly, WebGL, and WebGPU that give us low-level access to hardware
-    across platforms. <span class="italic"
-      >(Performance benchmarks coming soon—expect <code>jax-js</code> to outperform
-      TensorFlow.js and others.)</span
+    WebAssembly, WebGL, and WebGPU that give us low-level access to hardware, as
+    ML models also become smaller and faster.
+  </p>
+
+  <p class="mb-5">
+    <span class="italic"
+      >XXX (Performance benchmarks coming soon—though expect <code>jax-js</code>
+      to be faster than TensorFlow.js due to JIT.)</span
     >
   </p>
 
   <p class="mb-5">
-    Want to distribute software? Make a website. And if you want to run neural
-    networks or numerical code on that website, use <code>jax-js</code>.
+    Running GPU code should be as easy as making a website. With <code
+      >jax-js</code
+    >, you can bring numerical computing to your users in the browser, with zero
+    installs or setup.
   </p>
 
   <p>
